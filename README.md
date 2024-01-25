8de678cc84881ecc251a8c92ab872ad206c4620a
CuongCter <cuongcter442002@gmail.com>
2024-01-25 13:12:18 +0700

    U7 Rendering List

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-# Props
-
-- Các thành phần React Sử dụng props để giao tiếp với nhau. Mỗi thành phần cha có thể chuyển một số thống tin đến các thành phần con của nó bằng cách cung cấp cho chúng các props. Có thể chuyển bất kỳ giá trị JS nào qua chúng, bao gồm các đối tượng, mảng và hàm.
-
-# Props children
-
-- Trong ReactJS, "props children" thường là một thuộc tính đặc biệt trong các component. Nó cho phép bạn truyền vào một hoặc nhiều thành phần con (children) giữa các thẻ mở và đóng của một component trong JSX.
+# Rendering list
--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,75 @@
-
-import './App.css'
-import Button from './components/Button'
-import ParentComponents from './components/ParentComponents'
-import Profile from './components/Profile'
+import "./App.css";
+import { people } from "./data.js";
+import { getImageUrl } from "./utils.js";
 
 function App() {
+  // const people = [
+  //   'Creola Katherine Johnson: mathematician',
+  //   'Mario José Molina-Pasquel Henríquez: chemist',
+  //   'Mohammad Abdus Salam: physicist',
+  //   'Percy Lavon Julian: chemist',
+  //   'Subrahmanyan Chandrasekhar: astrophysicist'
+  // ];
 
+  // const people = [{
+  //   id: 0,
+  //   name: 'Creola Katherine Johnson',
+  //   profession: 'mathematician',
+  // }, {
+  //   id: 1,
+  //   name: 'Mario José Molina-Pasquel Henríquez',
+  //   profession: 'chemist',
+  // }, {
+  //   id: 2,
+  //   name: 'Mohammad Abdus Salam',
+  //   profession: 'physicist',
+  // }, {
+  //   name: 'Percy Lavon Julian',
+  //   profession: 'chemist',
+  // }, {
+  //   name: 'Subrahmanyan Chandrasekhar',
+  //   profession: 'astrophysicist',
+  // }];
+
+  // const listItems = people.map(person => <li>{person}</li>);
+
+  // const chemists = people.filter(person =>
+  //   person.profession === 'chemist'
+  // );
+
+  // const listItems = chemists.map(person =>
+  //   <li>
+  //      <img
+  //        alt={person.name}
+  //      />
+  //      <p>
+  //        <b>{person.name}:</b>
+  //        {' ' + person.profession + ' '}
+  //        known for {person.accomplishment}
+  //      </p>
+  //   </li>
+  // );
+  const chemists = people.filter((person) => person.profession === "chemist");
+  const listItems = chemists.map((person,index) => (
+    <li key={index}>
+      <img src={getImageUrl(person)} alt={person.name} />
+      <p>
+        <b>{person.name}:</b>
+        {" " + person.profession + " "}
+        known for {person.accomplishment}
+      </p>
+    </li>
+  ));
   return (
     <>
-     <section>
-      <h1>Amazing scientists</h1>
-      <Profile color={`red`}/>
-      <Profile/>
-      <ParentComponents/>
-      <Button>Click hello</Button>
-    </section>
+      {
+        people.length > 0 ? people.map((person,index) =>(
+          <div key={person.id}>{person.name}</div>
+        )) : null 
+      } 
+      <ul>{listItems}</ul>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
